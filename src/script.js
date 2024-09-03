a0d1d44583ba0a4c9ac22eb1b08459b15ee8c846
kdinesh1234 <158142344+kdinesh1234@users.noreply.github.com>
2024-09-03 08:06:03 -0700

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,11 @@
+const getDetails = document.getElementById("getDetails");
+
+getDetails.addEventListener("click", async () => {
+  //!using the joke api to fetch the data from server.
+  const result = await fetch("https://official-joke-api.appspot.com/random_joke");
+  const data = await result.json();
+  console.log(data);
+  const output = document.getElementById("api1");
+  output.innerHTML = `💥💥💥💥Click the above Jokes button the content will be changed 💥💥💥💥
+    <br>  jokes ::  ${data.setup} <br> punch ::${data.punchline} <br>`;
+});
